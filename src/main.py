d7636af2120e99108341b948a64b48c5858edcfe
Logan Pappas <loganpappasse@gmail.com>
2025-11-19 19:48:51 -0600

    ok im done now

--- src/main.py
+++ src/main.py
@@ -4,4 +4,5 @@ class main:
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={42}&lon={-93}&appid={api}"
     city = input("what city luh bro\n").lower()
     weather.load(api,city,url)
+
     weather.get_weather(city,api,url)
